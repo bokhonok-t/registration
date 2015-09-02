7ba8e3f3f957aa41f71465837d1d828e7c0f687d
bokhonok-t <tanyushab95@gmail.com>
2015-09-02 18:07:50 +0300

    Delete config.php

--- img/profiles/config.php
+++
@@ -1,8 +0,0 @@
-<?
-$server = 'localhost';
-$user = 'root';
-$password = '';
-$dblink = mysql_connect($server, $user, $password);
-$database = 'shop';
-$selected = mysql_select_db($database, $dblink);
-?>
